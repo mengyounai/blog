11405c3b687481120d6f5d329d9018d1c6b39cd5
无限缤纷 <2193873302@qq.com>
2022-07-31 22:06:19 +0800

    1、修复搜索会搜到删除数据

--- src/main/java/com/example/blog/web/IndexController.java
+++ src/main/java/com/example/blog/web/IndexController.java
@@ -32,8 +32,8 @@ public class IndexController {
         model.addAttribute("types", typeService.listTypeTop(6));
         model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogs", blogService.listRecommedBlogTop(8));
-//        return "index";
-        return "newDesign/indexNew";
+        return "index";
+//        return "newDesign/indexNew";
     }
 
 
@@ -44,6 +44,7 @@ public class IndexController {
         model.addAttribute("query", query);
         return "search";
     }
+
 
     @GetMapping("/blog/{id}")
     public String blog(@PathVariable Long id, Model model) {

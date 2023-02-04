18dbd50c2cf305c70e983fe416bbbeb72278896a
无限缤纷 <2193873302@qq.com>
2023-02-04 14:27:23 +0800

    1、解决图床上传图片重名问题

--- src/main/java/com/example/blog/po/Blog.java
+++ src/main/java/com/example/blog/po/Blog.java
@@ -3,6 +3,7 @@ package com.example.blog.po;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
--- src/test/java/com/example/blog/BlogApplicationTests.java
+++ src/test/java/com/example/blog/BlogApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.blog;
 
-import com.example.blog.po.GirlFriend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,14 +10,6 @@ class BlogApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    void createGirlFriend() {
-        GirlFriend girlFriend=new GirlFriend();
-        girlFriend.setName("如花");
-        girlFriend.setType("肤白貌美大长腿，温柔体贴又多金");
-        System.out.println("恭喜！成功new出了女朋友！女朋友的属性为...");
-        System.out.println("姓名："+girlFriend.getName());
-        System.out.println("属性："+girlFriend.getType());
-    }
+
 
 }
